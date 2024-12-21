cfcbd170c2b9f5e63b53e5bf2a04b38a4f7fcb9b
sedlarevic <raldesedlar@gmail.com>
2024-12-21 17:13:04 +0100

    added parsers for null and bool, added constants for jsonbool and jsonnull

--- src/json_parser/core.clj
+++ src/json_parser/core.clj
@@ -2,30 +2,32 @@
   (:require
    [clojure.string :refer [blank? join]]))
 
+;records
 (defrecord JsonString [value])
 (defrecord JsonNumber [value]) ; NOTE/TODO: no support for float values 
 (defrecord JsonBool [value])
-(defrecord JsonNull [])
+(defrecord JsonNull [value])
 (defrecord JsonArray [value]) ; should be an array 
 (defrecord JsonObject [value]) ; should be a map
 
-(defrecord Parser [run-parser])
-(defn make-parser [parser-fn]
-  (->Parser parser-fn))
+;instances
+(def json-bool-true (->JsonBool true))
+(def json-bool-false (->JsonBool false))
+(def json-null (->JsonNull nil))
 ;takes first letter of the string-value, and checks if the letter is the expected char
 ;if correct, returns the char and the rest of string, if not returns nil
 (defn parse-char [string-val expected-char]
   (if (blank? string-val)
     (do (println (blank? string-val) expected-char) nil)
     (do
-      (println "radi..." string-val expected-char)
+      (println "parsing char..." string-val expected-char)
       (let [first-char (first string-val)]
         (if (= expected-char first-char)
           (do
             (let [rest-of-string (subs (str string-val) 1)]
               [first-char rest-of-string]))
           (do
-            (println "ne gadjaju se char-ovi")
+            (println "chars are not same" first-char expected-char)
             nil))))))
 ;recursively goes through expected-string-value
 ;when expected-string-value is blank, we return expected value, and the rest of the string-val 
@@ -39,3 +41,18 @@
         (if (nil? parsed-char)
           nil
           (recur (subs expected-string 1) rest-of-string (conj output parsed-char)))))))
+
+(defn parse-jsonNull [string-val]
+  (let [[output remaining-string] (parse-string string-val "null")]
+    (if (nil? output)
+      nil
+      [json-null remaining-string])))
+
+(defn parse-jsonBool [string-val]
+  (let [[output-for-true remaining-string-for-true] (parse-string string-val "true")]
+    (if (nil? output-for-true)
+      (do (let [[output-for-false remaining-string-for-false] (parse-string string-val "false")]
+            (if (nil? output-for-false)
+              nil
+              [json-bool-false remaining-string-for-false])))
+      [json-bool-true remaining-string-for-true])))

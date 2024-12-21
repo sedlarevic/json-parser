3a87ecfe1ea39a29f0b28f22eaa9a65d32109c50
sedlarevic <raldesedlar@gmail.com>
2024-12-21 02:14:16 +0100

    fixed bug where string with false in it was not parsing correctly

--- src/json_parser/core.clj
+++ src/json_parser/core.clj
@@ -16,18 +16,17 @@
 ;if correct, returns the char and the rest of string, if not returns nil
 (defn parse-char [string-val expected-char]
   (if (blank? string-val)
-    nil
+    (do (println (blank? string-val) expected-char) nil)
     (do
-      (println "Not an empty string!")
+      (println "radi..." string-val expected-char)
       (let [first-char (first string-val)]
         (if (= expected-char first-char)
           (do
             (let [rest-of-string (subs (str string-val) 1)]
               [first-char rest-of-string]))
           (do
+            (println "ne gadjaju se char-ovi")
             nil))))))
-;NOTE: DOES NOT WORK FOR: "falseasdasd" "false", but works for "trueasdasd" "true"
-;TODO: Find a fix for a bug
 ;recursively goes through expected-string-value
 ;when expected-string-value is blank, we return expected value, and the rest of the string-val 
 (defn parse-string [string-val expected-string-val]
@@ -39,4 +38,4 @@
       (let [[parsed-char rest-of-string] (parse-char remaining-string (first expected-string))]
         (if (nil? parsed-char)
           nil
-          (recur rest-of-string (subs expected-string 1) (conj output parsed-char)))))))
+          (recur (subs expected-string 1) rest-of-string (conj output parsed-char)))))))
